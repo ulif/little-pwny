9de5f279b7e19f8d0540c16aff24497acfe36e67
ulif <uli@gnufix.de>
2019-09-28 04:06:23 +0200

    First, plain implementation.
    Still no user interface.

--- pwpwnd/__init__.py
+++ pwpwnd/__init__.py
@@ -1,3 +1,25 @@
-# 
-# make this a package
-# 
+import hashlib
+import http.client
+
+
+def mkhash(password):
+    """Create SHA1 hash from `password`.
+
+    HIBP API requests SHA1 hashed password.
+    """
+    return hashlib.sha1(
+            password.encode('utf-8')).hexdigest().upper()
+
+
+def num_pwned(hash_val):
+    """Lookup haveibeenpwned.com for `hash_val`.
+
+    Returns the number of entries found starting with the first 5 chars of
+    `hash_val`.
+    """
+    conn = http.client.HTTPSConnection("api.pwnedpasswords.com")
+    conn.request("GET", "/range/%s" % hash_val[:5])
+    resp = conn.getresponse()
+    entries = resp.read().decode('utf-8').split('\r\n')
+    result = dict([x.split(':') for x in entries]).get(hash_val[5:], 0)
+    return resp.status, int(result)
