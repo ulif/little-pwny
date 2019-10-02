34b7a1a83c6e73640388d85548ce83d98a694654
ulif <uli@gnufix.de>
2019-10-02 10:15:03 +0200

    Rename package.
    and update setup.py accordingly.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-        name="pwpwnd",
+        name="pwny",
         version="0.1.dev0",
         description="Check whether your password has been pwnd",
-        url="https://github.com/ulif/pwpwnd",
+        url="https://github.com/ulif/pwny",
         author="ulif",
         author_email="uli@gnufix.de",
         license="GPL3",
-        packages=["pwpwnd"],
+        packages=["pwny"],
         zip_safe=False,
         entry_points={
-            'console_scripts': ['pwpwnd = pwpwnd:main']}
+            'console_scripts': ['pwpwnd = pwny:main']}
         )
 
