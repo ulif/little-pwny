995f682c2b9e0538be0c7d3c847da3ef4cde7122
ulif <uli@gnufix.de>
2019-10-12 23:45:53 +0200

    Add trove classifiers.

--- setup.py
+++ setup.py
@@ -8,6 +8,21 @@ setup(
         author="ulif",
         author_email="uli@gnufix.de",
         license="GPL3",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Environment :: Console",
+            "Intended Audience :: End Users/Desktop",
+            "Intended Audience :: System Administrators",
+            "Topic :: Utilities",
+            "Topic :: Security :: Cryptography",
+            (
+                "License :: OSI Approved :: "
+                "GNU General Public License v3 or later (GPLv3+)"),
+                "Operating System :: POSIX :: Linux",
+                "Programming Language :: Python :: 3",
+                "Programming Language :: Python :: 3.6",
+                "Programming Language :: Python :: Implementation :: CPython",
+        ],
         packages=["pwny"],
         zip_safe=False,
         entry_points={
