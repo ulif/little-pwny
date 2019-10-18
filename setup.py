551545f0df8e0db9e52d50c9dd4802947ce66ada
ulif <uli@gnufix.de>
2019-10-18 18:34:08 +0200

    Changed distribution name.
    `pwny` is taken already on pypi.org. `little-pwny` is better in the
    regard, that it stresses one characteristic quality of the package: it
    is little. Furhtermore, little pwnies are simply cuter.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-        name="pwny",
+        name="little-pwny",
         version="0.1.dev0",
         description="Check whether your password has been pwnd",
-        url="https://github.com/ulif/pwny",
+        url="https://github.com/ulif/little-pwny",
         author="ulif",
         author_email="uli@gnufix.de",
         license="GPL3",
@@ -26,5 +26,5 @@ setup(
         packages=["pwny"],
         zip_safe=False,
         entry_points={
-            'console_scripts': ['pwny = pwny:main']}
+            'console_scripts': ['little-pwny = pwny:main']}
         )
