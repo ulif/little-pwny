41042313449ccf82e6d2515a250bf859cf50716c
ulif <uli@gnufix.de>
2019-11-06 17:51:18 +0100

    Also tests want comments.

--- tests/test_pwny.py
+++ tests/test_pwny.py
@@ -2,16 +2,19 @@ from pwny import mkhash, num_pwned, main
 
 
 def test_mkhash():
+    # Ensure, we get valid SHA1 sums
     assert mkhash("P@ssw0rd") == "21BD12DC183F740EE76F27B78EB39C8AD972A757"
 
 
 def test_num_pwned():
+    # We get a status and a number from `num_pwned`
     status, num = num_pwned("21BD12DC183F740EE76F27B78EB39C8AD972A757")
     assert status == 200
     assert num > 52500
 
 
 def test_main(capsys):
+    # main outputs a number, regularily
     main("P@ssw0rd")
     out, err = capsys.readouterr()
     assert int(out) > 52000
