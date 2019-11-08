5fac1178c0c94273a4192fdf53a3b9562d3a9b6f
ulif <uli@gnufix.de>
2019-11-08 08:01:27 +0100

    Try to enable offline testing.
    We replace relevant network functions with fake versions (for tests
    only), that mimic the answers we got from haveibeenpwned.com.

--- tests/test_pwny.py
+++ tests/test_pwny.py
@@ -1,26 +1,45 @@
+import os
+import pytest
 from pwny import mkhash, num_pwned, main
 
 
+def faux_urlopen(url):
+    # A replacement for `urllib.request.urlopen`
+    path = os.path.join(
+        os.path.dirname(__file__),
+        "21BD12DC183F740EE76F27B78EB39C8AD972A757.hash")
+    f = open(path, "rb")
+    f.status = 200
+    return f
+
+
+@pytest.fixture(scope="function")
+def offline(request, monkeypatch):
+    # Replace network functions we use in tests with fake ones.
+    monkeypatch.setattr(
+            "urllib.request.urlopen", faux_urlopen)
+
+
 def test_mkhash():
-    # Ensure, we get valid SHA1 sums
+    # the hashes we build are regular sha1 hashes
     assert mkhash("P@ssw0rd") == "21BD12DC183F740EE76F27B78EB39C8AD972A757"
 
 
-def test_num_pwned():
-    # We get a status and a number from `num_pwned`
+def test_num_pwned(offline):
+    # we get expectable numbers as results
     status, num = num_pwned("21BD12DC183F740EE76F27B78EB39C8AD972A757")
     assert status == 200
     assert num > 52500
 
 
-def test_main(capsys):
-    # main outputs a number, regularily
+def test_main(capsys, offline):
+    # we can run the main function w/o trouble
     main("P@ssw0rd")
     out, err = capsys.readouterr()
     assert int(out) > 52000
 
 
-def test_main_none(capsys, monkeypatch):
+def test_main_none(capsys, offline, monkeypatch):
     # sys.argv is used if no arg was passed in
     monkeypatch.setattr("sys.argv", ["<scriptname>", "P@ssw0rd"])
     main()
