ba4c5a1273a4fcfe52b26bf9fe660f5f0a6b1c8a
ulif <uli@gnufix.de>
2019-12-04 08:26:29 +0100

    Fix typo.

--- pwny/__init__.py
+++ pwny/__init__.py
@@ -12,7 +12,7 @@ def print_version():
     """Output current version and other infos.
     """
     print("little-pwny %s" % __version__)
-    print("Copyrright (C) 2019 ulif")
+    print("Copyright (C) 2019 ulif")
 
 
 def handle_options(args):
