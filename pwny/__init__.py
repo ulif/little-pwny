1ba614e939561c9d23104c41bc8d228e024a6cef
ulif <uli@gnufix.de>
2019-12-07 08:39:04 +0100

    Display GPL with --version.

--- pwny/__init__.py
+++ pwny/__init__.py
@@ -8,11 +8,32 @@ from argparse import ArgumentParser
 __version__ = pkg_resources.get_distribution('little-pwny').version
 
 
+GPL_TEXT = (
+    """
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    """
+    )
+
+
 def print_version():
     """Output current version and other infos.
     """
     print("little-pwny %s" % __version__)
     print("Copyright (C) 2019 ulif")
+    print("little-pwny relies on data from haveibeenpwned.com.")
+    print("See https://haveibeenpwned.com/Passwords for details.")
+    print(GPL_TEXT)
 
 
 def handle_options(args):
